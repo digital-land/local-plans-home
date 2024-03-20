4fd464f6c4e9209ae4188ed283573855897d9328
Ronan Graham <Ro4052@users.noreply.github.com>
2024-03-20 17:01:12 +0000

    SOW-25: custom 404 (#12)
    * SOW-25: add custom 404 page
    
    * SOW-25: switch 404 to Nunjucks template
    
    * SOW-25: repoint links to non-existent pages
    
    * SOW-25: fix typos

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,36 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const path = require("path");
+const fs = require("fs");
+const router = govukPrototypeKit.requests.setupRouter();
 
-// Add your routes here
+const basePath = path.join(__dirname, "./views");
+
+const getAllRoutesFromDir = (dir, ignore = []) => {
+  const paths = fs
+    .readdirSync(path.join(basePath, dir))
+    .filter((p) => !ignore.includes(p));
+
+  return paths.flatMap((p) =>
+    fs.lstatSync(path.join(basePath, dir, p)).isDirectory()
+      ? getAllRoutesFromDir(path.join(dir, p))
+      : [`/${path.join(dir, p.replace(/.html$/, "")).replace(/\\/g, "/")}`]
+  );
+};
+
+const allAvailableRoutes = [
+  ...getAllRoutesFromDir(".", ["layouts", "404.html", "index.html"]),
+  "/",
+];
+
+router.use("*", (req, res, next) => {
+  if (!allAvailableRoutes.includes(req.originalUrl)) {
+    res.status(404).render(path.join(__dirname, "./views/404.html"), {
+      path: req.originalUrl,
+    });
+  } else {
+    next();
+  }
+});
