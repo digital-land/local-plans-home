6a91471e099acbe5f2273aa44ef7dbc28aa7a534
emmasaraha <emma.axelsson@tpximpact.com>
2024-10-11 14:02:42 +0100

    sow32 - set up of newest index for CULP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,13 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 
+  // Stop dummy prototype links from jumping to top
+  $("a").click(function() {
+  var dummyLink = $(this).attr("href");
+  if( dummyLink == "#" ) {
+    return false;
+  }
+});
+
+
 })
